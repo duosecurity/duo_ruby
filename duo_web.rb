087d1d3b247bb6a107f1052b3b5148b175a38b17
Matt Schwager <matts@duosecurity.com>
2016-06-07 14:40:44 -0400

    Linting of Ruby files

--- duo_web.rb
+++ duo_web.rb
@@ -1,96 +1,107 @@
 require 'openssl'
 require 'base64'
 
+##
+# A Ruby implementation of the Duo WebSDK
+#
 module Duo
+  DUO_PREFIX  = 'TX'.freeze
+  APP_PREFIX  = 'APP'.freeze
+  AUTH_PREFIX = 'AUTH'.freeze
 
-	DUO_PREFIX  = 'TX'
-	APP_PREFIX  = 'APP'
-	AUTH_PREFIX = 'AUTH'
+  DUO_EXPIRE = 300
+  APP_EXPIRE = 3600
 
-	DUO_EXPIRE = 300
-	APP_EXPIRE = 3600
+  IKEY_LEN = 20
+  SKEY_LEN = 40
+  AKEY_LEN = 40
 
-	IKEY_LEN = 20
-	SKEY_LEN = 40
-	AKEY_LEN = 40
+  ERR_USER = 'ERR|The username passed to sign_request() is invalid.'.freeze
+  ERR_IKEY = 'ERR|The Duo integration key passed to sign_request() is invalid.'.freeze
+  ERR_SKEY = 'ERR|The Duo secret key passed to sign_request() is invalid.'.freeze
+  ERR_AKEY = "ERR|The application secret key passed to sign_request() must be at least #{Duo::AKEY_LEN} characters.".freeze
 
-	ERR_USER = 'ERR|The username passed to sign_request() is invalid.'
-	ERR_IKEY = 'ERR|The Duo integration key passed to sign_request() is invalid.'
-	ERR_SKEY = 'ERR|The Duo secret key passed to sign_request() is invalid.'
-	ERR_AKEY = "ERR|The application secret key passed to sign_request() must be at least #{Duo::AKEY_LEN} characters."
+  # Sign a Duo 2FA request
+  # @param ikey [String] The Duo IKEY
+  # @param skey [String] The Duo SKEY
+  # @param akey [String] The Duo AKEY
+  # @param username [String] Username to authenticate as
+  def sign_request(ikey, skey, akey, username)
+    return Duo::ERR_USER if !username || username.empty?
+    return Duo::ERR_USER if username.include? '|'
+    return Duo::ERR_IKEY if !ikey || ikey.to_s.length != Duo::IKEY_LEN
+    return Duo::ERR_SKEY if !skey || skey.to_s.length != Duo::SKEY_LEN
+    return Duo::ERR_AKEY if !akey || akey.to_s.length < Duo::AKEY_LEN
 
-	# Sign a Duo request with the ikey, skey, akey, and username
-	def sign_request(ikey, skey, akey, username)
-		return Duo::ERR_USER if not username or username.to_s.length == 0
-		return Duo::ERR_USER if username.include? '|'
-		return Duo::ERR_IKEY if not ikey or ikey.to_s.length != Duo::IKEY_LEN
-		return Duo::ERR_SKEY if not skey or skey.to_s.length != Duo::SKEY_LEN
-		return Duo::ERR_AKEY if not akey or akey.to_s.length < Duo::AKEY_LEN
+    vals = [username, ikey]
 
-		vals = [username, ikey]
+    duo_sig = sign_vals(skey, vals, Duo::DUO_PREFIX, Duo::DUO_EXPIRE)
+    app_sig = sign_vals(akey, vals, Duo::APP_PREFIX, Duo::APP_EXPIRE)
 
-		duo_sig = sign_vals(skey, vals, Duo::DUO_PREFIX, Duo::DUO_EXPIRE)
-		app_sig = sign_vals(akey, vals, Duo::APP_PREFIX, Duo::APP_EXPIRE)
+    return [duo_sig, app_sig].join(':')
+  end
 
-		return [duo_sig, app_sig].join(':')
-	end
+  # Verify a Duo 2FA request
+  # @param ikey [String] The Duo IKEY
+  # @param skey [String] The Duo SKEY
+  # @param akey [String] The Duo AKEY
+  # @param sig_response [String] Response from Duo service
+  def verify_response(ikey, skey, akey, sig_response)
+    begin
+      auth_sig, app_sig = sig_response.to_s.split(':')
+      auth_user = parse_vals(skey, auth_sig, Duo::AUTH_PREFIX, ikey)
+      app_user = parse_vals(akey, app_sig, Duo::APP_PREFIX, ikey)
+    rescue
+      return nil
+    end
 
-	# Verify a response from Duo with the skey and akey.
-	def verify_response(ikey, skey, akey, sig_response)
-		begin
-			auth_sig, app_sig = sig_response.to_s.split(':')
-			auth_user = parse_vals(skey, auth_sig, Duo::AUTH_PREFIX, ikey)
-			app_user = parse_vals(akey, app_sig, Duo::APP_PREFIX, ikey)
-		rescue
-			return nil
-		end
+    return nil if auth_user != app_user
 
-		return nil if auth_user != app_user
+    return auth_user
+  end
 
-		return auth_user
-	end
-  
-	private
-	def hmac_sha1(key, data)
-		OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'), key, data.to_s)
-	end
+  private
 
-	def sign_vals(key, vals, prefix, expire)
-		exp = Time.now.to_i + expire
+  def hmac_sha1(key, data)
+    OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), key, data.to_s)
+  end
 
-		val_list = vals + [exp]
-		val = val_list.join('|')
+  def sign_vals(key, vals, prefix, expire)
+    exp = Time.now.to_i + expire
 
-		b64 = Base64.encode64(val).gsub(/\n/,'')
-		cookie = prefix + '|' + b64
+    val_list = vals + [exp]
+    val = val_list.join('|')
 
-		sig = hmac_sha1(key, cookie)
-		return [cookie, sig].join('|')
-	end
+    b64 = Base64.encode64(val).delete("\n")
+    cookie = prefix + '|' + b64
 
-	def parse_vals(key, val, prefix, ikey)
-		ts = Time.now.to_i
+    sig = hmac_sha1(key, cookie)
+    return [cookie, sig].join('|')
+  end
 
-		parts = val.to_s.split('|')
-		return nil if parts.length !=3
-		u_prefix, u_b64, u_sig = parts
+  def parse_vals(key, val, prefix, ikey)
+    ts = Time.now.to_i
 
-		sig = hmac_sha1(key, [u_prefix, u_b64].join('|'))
+    parts = val.to_s.split('|')
+    return nil if parts.length != 3
+    u_prefix, u_b64, u_sig = parts
 
-		return nil if hmac_sha1(key, sig) != hmac_sha1(key, u_sig)
+    sig = hmac_sha1(key, [u_prefix, u_b64].join('|'))
 
-		return nil if u_prefix != prefix
+    return nil if hmac_sha1(key, sig) != hmac_sha1(key, u_sig)
 
-		cookie_parts = Base64.decode64(u_b64).to_s.split('|')
-		return nil if cookie_parts.length != 3
-		user, u_ikey, exp = cookie_parts
+    return nil if u_prefix != prefix
 
-		return nil if u_ikey != ikey
+    cookie_parts = Base64.decode64(u_b64).to_s.split('|')
+    return nil if cookie_parts.length != 3
+    user, u_ikey, exp = cookie_parts
 
-		return nil if ts >= exp.to_i
+    return nil if u_ikey != ikey
 
-		return user
-	end
+    return nil if ts >= exp.to_i
 
-	extend self
+    return user
+  end
+
+  extend self
 end
--- test.rb
+++ test.rb
@@ -1,66 +1,69 @@
 require 'test/unit'
-require 'duo_web'
+require_relative 'duo_web'
 
-IKEY = "DIXXXXXXXXXXXXXXXXXX"
-WRONG_IKEY = "DIXXXXXXXXXXXXXXXXXY"
-SKEY = "deadbeefdeadbeefdeadbeefdeadbeefdeadbeef"
-AKEY = "useacustomerprovidedapplicationsecretkey"
+IKEY = 'DIXXXXXXXXXXXXXXXXXX'.freeze
+WRONG_IKEY = 'DIXXXXXXXXXXXXXXXXXY'.freeze
+SKEY = 'deadbeefdeadbeefdeadbeefdeadbeefdeadbeef'.freeze
+AKEY = 'useacustomerprovidedapplicationsecretkey'.freeze
 
-USER = "testuser"
+USER = 'testuser'.freeze
 
-INVALID_RESPONSE = "AUTH|INVALID|SIG"
-EXPIRED_RESPONSE = "AUTH|dGVzdHVzZXJ8RElYWFhYWFhYWFhYWFhYWFhYWFh8MTMwMDE1Nzg3NA==|cb8f4d60ec7c261394cd5ee5a17e46ca7440d702"
-FUTURE_RESPONSE = "AUTH|dGVzdHVzZXJ8RElYWFhYWFhYWFhYWFhYWFhYWFh8MTYxNTcyNzI0Mw==|d20ad0d1e62d84b00a3e74ec201a5917e77b6aef"
-WRONG_PARAMS_RESPONSE = "AUTH|dGVzdHVzZXJ8RElYWFhYWFhYWFhYWFhYWFhYWFh8MTYxNTcyNzI0M3xpbnZhbGlkZXh0cmFkYXRh|6cdbec0fbfa0d3f335c76b0786a4a18eac6cdca7"
-WRONG_PARAMS_APP = "APP|dGVzdHVzZXJ8RElYWFhYWFhYWFhYWFhYWFhYWFh8MTYxNTcyNzI0M3xpbnZhbGlkZXh0cmFkYXRh|7c2065ea122d028b03ef0295a4b4c5521823b9b5"
+INVALID_RESPONSE = 'AUTH|INVALID|SIG'.freeze
+EXPIRED_RESPONSE = 'AUTH|dGVzdHVzZXJ8RElYWFhYWFhYWFhYWFhYWFhYWFh8MTMwMDE1Nzg3NA==|cb8f4d60ec7c261394cd5ee5a17e46ca7440d702'.freeze
+FUTURE_RESPONSE = 'AUTH|dGVzdHVzZXJ8RElYWFhYWFhYWFhYWFhYWFhYWFh8MTYxNTcyNzI0Mw==|d20ad0d1e62d84b00a3e74ec201a5917e77b6aef'.freeze
+WRONG_PARAMS_RESPONSE = 'AUTH|dGVzdHVzZXJ8RElYWFhYWFhYWFhYWFhYWFhYWFh8MTYxNTcyNzI0M3xpbnZhbGlkZXh0cmFkYXRh|6cdbec0fbfa0d3f335c76b0786a4a18eac6cdca7'.freeze
+WRONG_PARAMS_APP = 'APP|dGVzdHVzZXJ8RElYWFhYWFhYWFhYWFhYWFhYWFh8MTYxNTcyNzI0M3xpbnZhbGlkZXh0cmFkYXRh|7c2065ea122d028b03ef0295a4b4c5521823b9b5'.freeze
 
+##
+# Test Duo Ruby WebSDK implementation
+#
 class TestDuo < Test::Unit::TestCase
-	def test_sign_request()
-		request_sig = Duo.sign_request(IKEY, SKEY, AKEY, USER)
-		assert_not_equal(request_sig, nil)
+  def test_sign_request
+    request_sig = Duo.sign_request(IKEY, SKEY, AKEY, USER)
+    assert_not_equal(request_sig, nil)
 
-		request_sig = Duo.sign_request(IKEY, SKEY, AKEY, '')
-		assert_equal(request_sig, Duo::ERR_USER)
+    request_sig = Duo.sign_request(IKEY, SKEY, AKEY, '')
+    assert_equal(request_sig, Duo::ERR_USER)
 
-		request_sig = Duo.sign_request(IKEY, SKEY, AKEY, 'in|valid')
-		assert_equal(request_sig, Duo::ERR_USER)
+    request_sig = Duo.sign_request(IKEY, SKEY, AKEY, 'in|valid')
+    assert_equal(request_sig, Duo::ERR_USER)
 
-		request_sig = Duo.sign_request('invalid', SKEY, AKEY, USER)
-		assert_equal(request_sig, Duo::ERR_IKEY)
+    request_sig = Duo.sign_request('invalid', SKEY, AKEY, USER)
+    assert_equal(request_sig, Duo::ERR_IKEY)
 
-		request_sig = Duo.sign_request(IKEY, 'invalid', AKEY, USER)
-		assert_equal(request_sig, Duo::ERR_SKEY)
+    request_sig = Duo.sign_request(IKEY, 'invalid', AKEY, USER)
+    assert_equal(request_sig, Duo::ERR_SKEY)
 
-		request_sig = Duo.sign_request(IKEY, SKEY, 'invalid', USER)
-		assert_equal(request_sig, Duo::ERR_AKEY)
-	end
+    request_sig = Duo.sign_request(IKEY, SKEY, 'invalid', USER)
+    assert_equal(request_sig, Duo::ERR_AKEY)
+  end
 
-	def test_verify_response()
-		request_sig = Duo.sign_request(IKEY, SKEY, AKEY, USER)
-		duo_sig, valid_app_sig = request_sig.to_s.split(':')
+  def test_verify_response
+    request_sig = Duo.sign_request(IKEY, SKEY, AKEY, USER)
+    _duo_sig, valid_app_sig = request_sig.to_s.split(':')
 
-		request_sig = Duo.sign_request(IKEY, SKEY, 'invalid' * 6, USER)
-		duo_sig, invalid_app_sig = request_sig.to_s.split(':')
+    request_sig = Duo.sign_request(IKEY, SKEY, 'invalid' * 6, USER)
+    _duo_sig, invalid_app_sig = request_sig.to_s.split(':')
 
-		invalid_user = Duo.verify_response(IKEY, SKEY, AKEY, INVALID_RESPONSE + ':' + valid_app_sig)
-		assert_equal(invalid_user, nil)
+    invalid_user = Duo.verify_response(IKEY, SKEY, AKEY, INVALID_RESPONSE + ':' + valid_app_sig)
+    assert_equal(invalid_user, nil)
 
-		expired_user = Duo.verify_response(IKEY, SKEY, AKEY, EXPIRED_RESPONSE + ':' + valid_app_sig)
-		assert_equal(expired_user, nil)
+    expired_user = Duo.verify_response(IKEY, SKEY, AKEY, EXPIRED_RESPONSE + ':' + valid_app_sig)
+    assert_equal(expired_user, nil)
 
-		future_user = Duo.verify_response(IKEY, SKEY, AKEY, FUTURE_RESPONSE + ':' + invalid_app_sig)
-		assert_equal(future_user, nil)
+    future_user = Duo.verify_response(IKEY, SKEY, AKEY, FUTURE_RESPONSE + ':' + invalid_app_sig)
+    assert_equal(future_user, nil)
 
-		future_user = Duo.verify_response(IKEY, SKEY, AKEY, FUTURE_RESPONSE + ':' + valid_app_sig)
-		assert_equal(future_user, USER)
+    future_user = Duo.verify_response(IKEY, SKEY, AKEY, FUTURE_RESPONSE + ':' + valid_app_sig)
+    assert_equal(future_user, USER)
 
-		future_user = Duo.verify_response(IKEY, SKEY, AKEY, FUTURE_RESPONSE + ':' + WRONG_PARAMS_APP)
-		assert_equal(future_user, nil)
+    future_user = Duo.verify_response(IKEY, SKEY, AKEY, FUTURE_RESPONSE + ':' + WRONG_PARAMS_APP)
+    assert_equal(future_user, nil)
 
-		future_user = Duo.verify_response(IKEY, SKEY, AKEY, WRONG_PARAMS_RESPONSE + ':' + valid_app_sig)
-		assert_equal(future_user, nil)
+    future_user = Duo.verify_response(IKEY, SKEY, AKEY, WRONG_PARAMS_RESPONSE + ':' + valid_app_sig)
+    assert_equal(future_user, nil)
 
-		future_user = Duo.verify_response(WRONG_IKEY, SKEY, AKEY, FUTURE_RESPONSE + ':' + valid_app_sig)
-		assert_equal(future_user, nil)
-	end
+    future_user = Duo.verify_response(WRONG_IKEY, SKEY, AKEY, FUTURE_RESPONSE + ':' + valid_app_sig)
+    assert_equal(future_user, nil)
+  end
 end
